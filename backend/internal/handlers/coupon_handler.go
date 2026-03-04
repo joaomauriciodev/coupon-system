cecd762eea580522be05bb52469e6d38b6428444
João Mauricio <joaomauricio.novigoit@fujioka.inf.br>
2026-03-03 22:15:13 -0300

    feat: add create coupon handler

---
+++ backend/internal/handlers/coupon_handler.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"coupon-system/internal/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type CouponHandler struct {
+	DB *gorm.DB
+}
+
+func NewCouponHandler(db *gorm.DB) *CouponHandler {
+	return &CouponHandler{DB: db}
+}
+
+func (h *CouponHandler) CreateCoupon(c *gin.Context) {
+	var coupon models.Coupon
+
+	if err := c.ShouldBindJSON(&coupon); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	coupon.Active = true
+
+	h.DB.Create(&coupon)
+
+	c.JSON(http.StatusCreated, coupon)
+}
